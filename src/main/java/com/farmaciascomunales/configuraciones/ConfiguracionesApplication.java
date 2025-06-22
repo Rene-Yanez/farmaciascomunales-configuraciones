97dbe160156392f58d0dbd8764ec16580f8bf402
Rene-Yanez <rene.yanez2001@alumnos.ubiobio.cl>
2025-06-22 18:54:18 -0400

    configuraciones

---
+++ src/main/java/com/farmaciascomunales/configuraciones/ConfiguracionesApplication.java
@@ -0,0 +1,13 @@
+package com.farmaciascomunales.configuraciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfiguracionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguracionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmaciascomunales/configuraciones/ConfiguracionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciascomunales.configuraciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfiguracionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
